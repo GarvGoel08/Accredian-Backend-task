b1ea386cf027947ccd44163a3c9bffa0ecf1f111
Garv Goel <garvgoel2927@gmail.com>
2025-02-18 00:57:24 +0530

    CORS Fix

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const app = express();
 app.use(express.json()); 
 
 app.use(cors({
-    origin: "https://accrediantask-rho.vercel.app",
-}));
+    origin: "https://accrediantask-rho.vercel.app",  
+    methods: ["GET", "POST", "PUT", "DELETE"],          
+    allowedHeaders: ["Content-Type", "Authorization"], 
+    credentials: true, 
+  }));
+  
 
 app.use("/api/referrals", referRoutes);
 app.get("/", (req, res) => {
